acc1b25326b0333c07d68a623b5c769d81857a51
kaolsi <ruth96ortega@gmail.com>
2018-09-26 18:44:01 +0200

    versión inicial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const cervezas = require('./cervezas.json')
+const uniqueRandomArray = require('unique-random-array')
+
+module.exports = {
+  todas: cervezas,
+  alazar: uniqueRandomArray(cervezas)
+}
